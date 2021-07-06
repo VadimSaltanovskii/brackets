f186801d698c7c4ccd825a3299ab0f5c0ae9500c
V.Saltanovskiy <v.saltanovskiy@zmeke.com>
2021-07-06 23:28:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let loopCount = str.length
+
+  for (let i = 0; i < loopCount; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      str = str.replace(bracketsConfig[j].join(''), '')
+    }
+  }
+
+  return !str.length
+
 }
